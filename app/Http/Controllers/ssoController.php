079338cf0cbeb93109d47b61edd926747d38c7ed
IDCDEV03 <idcdev03@iddrives.co.th>
2024-05-14 15:08:47 +0700

    ีupdate

--- app/Http/Controllers/ssoController.php
+++ app/Http/Controllers/ssoController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Carbon\Carbon;
@@ -13,29 +14,26 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class ssoController extends Controller
 {
-   
-    
-    public function index($id,$user,$course)
+
+
+    public function index($id, $user, $course)
     {
         $userRows = DB::table('users')
-        ->where('email','=', $id)
-        ->get();
+            ->where('email', '=', $id)
+            ->get();
 
-        if($course == 'car')
-        {
-            $course_id = '2JD9LFPDNO';
-        }elseif($course == 'motobike')
-        {
-            $course_id = 'G2EAFFZ55F';
-        }elseif($course == 'trailer')
-        {
-            $course_id = 'Z0FQE1GKWN';
+        if ($course == 'car') {
+            $course_id = 'QNFS80B5SA';
+        } elseif ($course == 'motobike') {
+            $course_id = 'LH8YEZGBTK';
+        } elseif ($course == 'trailer') {
+            $course_id = '1DEQEYL3OW';
         }
 
-        if(count($userRows) == 0) {
+        if (count($userRows) == 0) {
             $user_id = Str::upper(Str::random(15));
             User::create([
-                'user_id'=> $user_id,
+                'user_id' => $user_id,
                 'name' => $user,
                 'email' => $id,
                 'password' => Hash::make($id),
@@ -44,26 +42,22 @@ class ssoController extends Controller
             ]);
 
             DB::table('user_details')->insert([
-                'user_id'=> $user_id,
+                'user_id' => $user_id,
                 'fullname' => $user,
                 'user_logo' => '0',
                 'user_status' => '1',
                 'user_dep' => $course_id,
                 'created_at' => Carbon::now()
             ]);
-            
-        }elseif(count($userRows) >= 1)
-        {
-            return view('login_sso',['user'=>$id]);
+        } elseif (count($userRows) >= 1) {
+            return view('login_sso', ['user' => $id]);
         }
-        
-        return view('login_sso',['user'=>$id]);
+
+        return view('login_sso', ['user' => $id]);
     }
 
     public function ssoLogin($user)
     {
-        return view('login_sso',['user'=>$user]);
+        return view('login_sso', ['user' => $user]);
     }
-  
-
 }
