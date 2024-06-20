ff03641f09ce30dec1a26709060c04c87c5d63e9
IDCDEV03 <idcdev03@iddrives.co.th>
2024-06-20 14:20:19 +0700

    update

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -33,11 +33,13 @@ class UserController extends Controller
     public function FormChk()
     {
         $agent_id = Auth::user()->user_dep;
+        $user_id =  Auth::user()->user_id;
 
         $formChk = DB::table('user_forms')
             ->join('form_chks', 'user_forms.form_id', '=', 'form_chks.form_id')          
             ->select('form_chks.form_name','form_chks.form_category','user_forms.*')
             ->where('user_forms.user_dep', '=', $agent_id)    
+             ->where('user_forms.user_id', '=', $user_id)  
             ->get();
 
         return view('user.formchk', compact('formChk'));
